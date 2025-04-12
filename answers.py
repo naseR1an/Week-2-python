c6004df46e987830dda0fea99ad7ad56ebb13ee4
naseR1an <joyserian8641@gmail.com>
2025-04-12 14:15:05 +0300

    week2 python assignment answers

--- answers.py
+++ answers.py
@@ -1,1 +1,22 @@
+# Empty list
+my_list=[]
+
+# Append to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#insert value 15 at the second position
+my_list.insert(1, 15)
+
+#remove the last element
+my_list.pop()
+
+# sort my_list in ascending order
+my_list.sort()
+
+# find and print the index of the value 30 in my_list
+index_of_30 = my_list.index(30)
+print("The index of 30 is:",index_of_30)
 
